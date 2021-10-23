ee8e5380074f24e613ff0e535f930e3ca1aecd15
minhquancn18 <minhquancn1809@gmail.com>
2021-10-23 22:27:24 +0700

    Start TourViewModel, will finish 24/10/2021

--- TourManagementSystem/TourManagementSystem/Managers/Model/TourModel.cs
+++ TourManagementSystem/TourManagementSystem/Managers/Model/TourModel.cs
@@ -4,35 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
+using TourManagementSystem.ViewModel;
 
 namespace TourManagementSystem.Managers.Model
 {
-    public class TourModel
+    public class TourModel : BaseViewModel
     {
-        private int _ID;
-        public int ID
-        {
-            get { return _ID; }
-            set { _ID = value; }
-        }
-        private string _TourName;
-        public string TourName
-        {
-            get { return _TourName; }
-            set { _TourName = value; }
-        }
-        private byte[] _IMAGETOUR;
-        public byte[] IMAGETOUR { get => _IMAGETOUR; set => _IMAGETOUR = value; }
+        private int _TOUR_ID;
+        public int TOUR_ID { get => _TOUR_ID; set { _TOUR_ID = value; OnPropertyChanged(); } }
 
-        private BitmapImage _IMAGE_SOURCE;
-        public BitmapImage IMAGE_SOURCE { get => _IMAGE_SOURCE; set { _IMAGE_SOURCE = value; } }
+        private string _TOUR_NAME;
+        public string TOUR_NAME { get => _TOUR_NAME; set { _TOUR_NAME = value; OnPropertyChanged(); } }
 
-        public string Host { get => _Host; set => _Host = value; }
-        public string TripLength { get => _TripLength; set => _TripLength = value; }
-        public string Price { get => _Price; set => _Price = value; }
+        private string _TOUR_CHARACTERISTIS;
+        public string TOUR_CHARACTERISTIS { get => _TOUR_CHARACTERISTIS; set { _TOUR_CHARACTERISTIS = value; OnPropertyChanged(); } }
 
-        private string _TripLength;
-        private string _Host;
-        private string _Price;
+        private string _TOUR_TYPE;
+        public string TOUR_TYPE { get => _TOUR_TYPE; set { _TOUR_TYPE = value; OnPropertyChanged(); } }
+
+        private string _TOUR_IS_EXIST;
+        public string TOUR_IS_EXIST { get => _TOUR_IS_EXIST; set { _TOUR_IS_EXIST = value; OnPropertyChanged(); } }
+
+        private byte[] _TOUR_IMAGE_BYTE_SOURCE;
+        public byte[] TOUR_IMAGE_BYTE_SOURCE { get => _TOUR_IMAGE_BYTE_SOURCE; set { _TOUR_IMAGE_BYTE_SOURCE = value; OnPropertyChanged(); } }
     }
 }
--- TourManagementSystem/TourManagementSystem/Managers/ViewModel/TourViewModel.cs
+++ TourManagementSystem/TourManagementSystem/Managers/ViewModel/TourViewModel.cs
@@ -1,14 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TourManagementSystem.Global.Model;
+using TourManagementSystem.Managers.Model;
 using TourManagementSystem.ViewModel;
 
 namespace TourManagementSystem.Managers.ViewModel
 {
     public class TourViewModel : BaseViewModel
     {
+        #region Data
+        #region Data Binding for TourUC
+        /*
+        * User is manager of system
+        */
+        private int _User_ID;
+        public int User_ID { get => _User_ID; set { _User_ID = value; OnPropertyChanged(); } }
 
+        /*
+         * TourList binding ItemSource on DataGridView of TourUC
+         */
+        private ObservableCollection<TourModel> _TourList;
+        public ObservableCollection<TourModel> TourList { get => _TourList; set { _TourList = value; OnPropertyChanged(); } }
+
+        /*
+         * Refresh_TourList is refresh TourList
+         */
+        private ObservableCollection<TourModel> _Refresh_TourList;
+        public ObservableCollection<TourModel> Refresh_TourList { get => _Refresh_TourList; set { _Refresh_TourList = value; OnPropertyChanged(); } }
+
+
+        /*
+         * Tour_Selected binding SelectedItem on DataGridView of TourUC
+         */
+        private TourModel _Tour_Selected;
+        public TourModel Tour_Selected
+        {
+            get => _Tour_Selected;
+            set
+            {
+                _Tour_Selected = value;
+                OnPropertyChanged();
+
+                /*
+                 * This step is insert select staff into display TourUC
+                 */
+                if (Tour_Selected != null)
+                {
+
+                }
+            }
+        }
+
+
+        /*
+        * Search_Tour_Text binding to Search button of TourUC
+        */
+        private string _Search_Tour_Text;
+        public string Search_Tour_Text
+        {
+            get => _Search_Tour_Text;
+            set
+            {
+                _Search_Tour_Text = value;
+                OnPropertyChanged();
+
+                /*
+                 * Step 1: Refresh TourList
+                 * Step 2: Check SearchText is null or not
+                 * Step 3: Check ComboBox is null or not
+                 * Step 4: Choose ComboBox type to select                 
+                 */
+                //Step 1
+                TourList = Refresh_TourList;
+
+                //Step 2
+                if (!string.IsNullOrEmpty(Search_Tour_Text))
+                {
+                    //Step 3
+                    if (CB_TourSelected != null)
+                    {
+                        //Step 4
+                        switch (CB_TourSelected.CB_Name)
+                        {
+                            case "Name":
+                                TourList = new ObservableCollection<TourModel>(TourList.Where(x => x.TOUR_NAME.Contains(Search_Tour_Text) ||
+                                                                                                        x.TOUR_NAME.ToLower().Contains(Search_Tour_Text) ||
+                                                                                                        x.TOUR_NAME.ToUpper().Contains(Search_Tour_Text)));
+                                break;
+                            case "Type":
+                                TourList = new ObservableCollection<TourModel>(TourList.Where(x => x.TOUR_TYPE.Contains(Search_Tour_Text) ||
+                                                                                                        x.TOUR_TYPE.ToLower().Contains(Search_Tour_Text) ||
+                                                                                                        x.TOUR_TYPE.ToUpper().Contains(Search_Tour_Text)));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        /*
+         * CB_TourList binding ItemSource of ComboBox of TourUC
+         */
+        private ObservableCollection<ComboBoxModel> _CB_TourList;
+        public ObservableCollection<ComboBoxModel> CB_TourList { get => _CB_TourList; set { _CB_TourList = value; OnPropertyChanged(); } }
+
+        /*
+         * CB_TourSelected binding SelectedValue of ComboBox of TourUC
+         */
+        private ComboBoxModel _CB_TourSelected;
+        public ComboBoxModel CB_TourSelected { get => _CB_TourSelected; set { _CB_TourSelected = value; OnPropertyChanged(); } }
+
+        #endregion Data Binding for TourUC
+        #endregion Data
     }
 }
