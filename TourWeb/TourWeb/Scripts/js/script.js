fcb25cbc18c730721469bfe4cb8db413bb0a7792
unknown <19522408@gm.uit.edu.vn>
2022-01-05 18:45:50 +0700

    Update Web

--- TourWeb/TourWeb/Scripts/js/script.js
+++ TourWeb/TourWeb/Scripts/js/script.js
@@ -15,25 +15,25 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     loginform.classList.remove('active');
 }
-menu.addEventListener('click', () =>{
+menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 })
-searchBtn.addEventListener('click', () =>{
+searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
 })
 
-formBtn.addEventListener('click', () =>{
+formBtn.addEventListener('click', () => {
     loginform.classList.add('active');
 })
 
-formClose.addEventListener('click', () =>{
+formClose.addEventListener('click', () => {
     loginform.classList.remove('active');
 })
 
-videoBtn.forEach(btn =>{
-    btn.addEventListener('click',() => {
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
         document.querySelector('.controls .active').classList.remove('active');
         btn.classList.add('active');
         let src = btn.getAttribute('data-src');
@@ -43,12 +43,12 @@ videoBtn.forEach(btn =>{
 
 var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
-    loop:true,
-    autoplay:{
+    loop: true,
+    autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
-    breakpoints:{
+    breakpoints: {
         640: {
             slidesPerView: 1,
         },
@@ -62,12 +62,12 @@ var swiper = new Swiper(".review-slider", {
 });
 var swiper = new Swiper(".brand-slider", {
     spaceBetween: 20,
-    loop:true,
-    autoplay:{
+    loop: true,
+    autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
-    breakpoints:{
+    breakpoints: {
         450: {
             slidesPerView: 2,
         },
@@ -81,4 +81,9 @@ var swiper = new Swiper(".brand-slider", {
             slidesPerView: 5,
         },
     }
+});
+
+$(function () {
+    $('#AlertBox').removeClass('hide');
+    $('#AlertBox').delay(2000).slideUp(500);
 });
